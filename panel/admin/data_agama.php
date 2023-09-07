60ec91d5d209bf7c5e7469ac9a7423a8bc8576e5
adamaprilod <adamaprilod@gmail.com>
2023-09-07 13:42:19 +0700

    nambah agama

---
+++ panel/admin/data_agama.php
@@ -0,0 +1,74 @@
+<?php include "header.php";
+if ($_SESSION['hak_akses'] != 'admin') {
+    echo "
+    <script>
+        alert('Tidak Memiliki Akses, DILARANG MASUK!');
+        document.location.href='index.php';
+    </script>
+    ";
+}
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+
+<body id="page-top">
+    <div class="container">
+        <div class="x_title">
+            <h2>Data <small>Agama</small></h2>
+    </div>
+    <div class="text-muted font-12 m-b-30 mb-2">
+        <a href="form_agama.php" type="button" class="btn btn-round btn-dark ml-2"><i class="fa fa-plus-circle" aria-hidden="true"></i> Tambah Data</a>
+    </div>
+    <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama Agama</th>
+                <th>Tgl Input</th>
+                <th>User Input</th>
+                <th>Tgl Update</th>
+                <th>User Update</th>
+                <th>Akses</th>
+                <th>Update</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            include 'conn.php';
+            $no = 1;
+            $query = "SELECT *
+            FROM agama
+            INNER JOIN user
+            ON agama.id_user = user.id_user";
+            $sql = mysqli_query($conn, $query);
+            while ($data = mysqli_fetch_assoc($sql)) {
+            ?>
+            <tr>
+                <td><?= $no++; ?></td>
+                <td><?= $data['nama_agama']; ?></td>
+                <td><?= $data['tgl_input']; ?></td>
+                <td><?= $data['user_input']; ?></td>
+                <td><?= $data['tgl_update']; ?></td>
+                <td><?= $data['user_update']; ?></td>
+                <td><?= $data['hak_akses']; ?> (<?= $data['nama']; ?>)</td>
+                <td><a class="btn btn-warning" type="button" href="edit_agama.php?id_agama=<?= $data['id_agama']; ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                <td><a class="btn btn-danger" type="button" onclick="return confirm('Data akan di Hapus?')" href="hapus_agama.php?id_agama=<?= $data['id_agama']; ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td>
+            </tr>
+        <?php
+            }
+        ?>
+        </tbody>
+    </table><br><br><br>
+    </div>
+    <!-- script -->
+    <script>
+        $(document).ready(function(){
+        new DataTable('#example');
+    })
+    </script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+    
+<?php include "footer.php";?>
---
+++ panel/admin/form_agama.php
@@ -0,0 +1,108 @@
+<?php
+include 'header.php';
+include 'conn.php';
+if ($_SESSION['hak_akses'] != 'admin') {
+    echo "
+    <script>
+        alert('Tidak Memiliki Akses, DILARANG MASUK!');
+        document.location.href='index.php';
+    </script>
+    ";
+}
+
+if (isset($_POST['simpan'])) {
+    $id_agama = htmlspecialchars($_POST['id_agama']);
+    $nama_agama = htmlspecialchars($_POST['nama_agama']);
+    $tgl_input = htmlspecialchars($_POST['tgl_input']);
+    $user_input = htmlspecialchars($_POST['user_input']);
+    $id_user = htmlspecialchars($_POST['id_user']);
+
+    //cek id sudah terdaftar belum
+    $result = mysqli_query($conn, "SELECT id_agama FROM agama WHERE id_agama = '$id_agama'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "
+        <script>
+            alert('ID sudah terdaftar, silahkan ganit!');
+            document.location.href='form_agama.php';
+        </script>
+        ";
+        return false;
+    }
+
+    mysqli_query($conn, "INSERT INTO agama VALUES('$id_agama','$nama_agama','$tgl_input','$user_input','','','$id_user')");
+
+    // var_dump($cek);
+    // exit();
+
+    if (mysqli_affected_rows($conn) > 0) {
+        echo "
+        <script>
+            alert('Data Agama Berhasil dibuat');
+            document.location.href='data_agama.php';
+        </script>
+        ";
+    } else {
+        echo "
+        <script>
+            alert('Data Agama Gagal dibuat');
+            document.location.href='form_agama.php';
+        </script>
+        ";
+    }
+}
+?>
+
+<body id="page-top">
+    <div class="container">
+    <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+                <div class="col-lg-2 d-none d-lg-block "></div>
+                <div class="col-lg-8">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Form Agama</h1>
+                        </div>
+                        <form class="user" method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="id_agama"
+                                placeholder="Id Agama" name="id_agama" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="nama_agama"
+                                    placeholder="Nama Agama" name="nama_agama" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="date" class="form-control form-control-user" id="tgl_input" name="tgl_input" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="user_input"
+                                    placeholder="User Input" name="user_input">
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control" name="id_user" id="id_user">
+                                    <option>Pilih Akses User</option>
+                                    <?php
+                                    $sql = mysqli_query($conn, "SELECT * FROM user WHERE hak_akses = '$status' AND id_user='$_SESSION[id_user];'");
+                                    while ($data = mysqli_fetch_assoc($sql)) {
+                                    ?>
+                                        <option value="<?= $data['id_user'] ?>"><?= $data['hak_akses'] ?> (<?= $data['nama'] ?>)</option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <button class="btn btn-danger" type="reset">Reset</button>
+                            <button type="submit" class="btn btn-primary" name="simpan">Simpan</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
